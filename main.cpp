9bbd499d9a57c9e820a43ab8034846fef1a2538c
njjza <njjza@hotmail.com>
2021-02-19 15:19:17 -0800

    Add main empty file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file        main.cpp
+ * @author      Henry Jiang njjza@github.io
+ * @version     0.1 2/19/2021
+ * 
+ * Description: This file is where the main function of the program at. It governs how to load data
+ *              into memory, and using methods DINEOF, and output the result to either a image or
+ *              to the console. The debugging method will also be in this file, and pre-runned
+ *              after the file is been updated.
+ * 
+**/
