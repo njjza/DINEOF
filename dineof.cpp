d2d0d1ee1f0b55d84d1b48c76cbf8a97b2c0925f
njjza <njjza@hotmail.com>
2021-02-18 17:40:57 -0800

    add DINEOF file

---
+++ dineof.cpp
@@ -0,0 +1,15 @@
+/**
+ * Author:  Henry Jiang
+ *          njjza@github.io
+ * 
+ * Description: The core file contains DINEOF algorithm. The current file is an realization of the
+ *              DIENEOF algorithm. It is an realization of the algorithm proposed in DINEOF (ref1)
+ *              
+ *              This algorithm has been shown to accurately determine Empirical Orthogonal Funct-
+ *              ions from gappy data sets that are used for data reconstructions. The return of
+ *              the method will is a fully interpolatede matrix which can then be subjected to a
+ *              final EOF decomposition.
+ * 
+ * Reference:   ref1: Beckers et.al. Data Interpolating Empirical Orthogonal Functions (DINEOF):
+ *                    a tool for geophysical data analyses. 2003
+**/
